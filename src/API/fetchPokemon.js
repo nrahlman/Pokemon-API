85d19a68bb8c9115c328d00190482ad926afd4b8
Nicky <nicholasaahlman@gmail.com>
2023-10-30 14:36:31 -0500

    Optimize Data Fetching, Reduce Fetch Requests and Add Dynamic Pagination

--- src/API/fetchPokemon.js
+++ src/API/fetchPokemon.js
@@ -4,7 +4,6 @@ export const fetchPokemon=async()=>{
 try {
     const response= await fetch(URL)
     const {results}=await response.json(); 
-    console.log(results)
     return results
     
 } catch (error) {
@@ -12,3 +11,14 @@ try {
 }   
 }
 
+export async function fetchPokemonData(url) {
+    try {
+        const response=await fetch (url)
+        const data= await response.json();
+        return data
+    } 
+ 
+    catch (error) {
+        console.log("Single pokemon not found")  
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,85 +1,112 @@
-import './App.css';
-import { fetchPokemon } from './API/fetchPokemon';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { fetchPokemon, fetchPokemonData } from "./API/fetchPokemon";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [pokemons, setPokemons]=useState([])
-  const [singlePokemon, setSinglePokemon]=useState([])
-  const [currentPage, setCurrentPage]=useState(1)
-  const pokemonPerPage = 5
+  const pokemonPerPage = 5;
+  const [pokemons, setPokemons] = useState([]);
+  const [currentPokemon, setCurrentPokemon] = useState(
+    pokemons.slice(0, pokemonPerPage)
+  );
+  const [singlePokemon, setSinglePokemon] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(()=>{
-    const getAllPokemon= async()=>{
-      const pokemons=await fetchPokemon();
-      setPokemons(pokemons)
-    }
+  useEffect(() => {
+    const getAllPokemon = async () => {
+      const pokemons = await fetchPokemon();
+      setPokemons(pokemons);
+    };
     getAllPokemon();
-    
-    console.log(pokemons)
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    async function GrabAllPokemon (url, index){
-      const fetchAllPokemon= await fetch(url)
-      const data= await fetchAllPokemon.json()
-    setSinglePokemon((pokemonArray)=>{
-      const newPokemonArray=[...pokemonArray]
-      newPokemonArray[index]=data
-      return newPokemonArray
-    })
-}
-pokemons.forEach((pokemon, index)=>{
-  GrabAllPokemon(pokemon.url, index)
-})
- 
-}, [pokemons])
-const indexOfLastPokemon= currentPage * pokemonPerPage
-const indexOfFirstPokemon= indexOfLastPokemon-pokemonPerPage
-const currentPokemon= singlePokemon.slice(indexOfFirstPokemon, indexOfLastPokemon)
+  useEffect(() => {
+    const updateCurrentPokemon = () => {
+      const firstPokemonIdx = (currentPage - 1) * pokemonPerPage;
+      const lastPokemonIdx = firstPokemonIdx + pokemonPerPage;
+      const pokemonToFetch = pokemons.slice(firstPokemonIdx, lastPokemonIdx);
+      setCurrentPokemon(pokemonToFetch);
+    };
 
-const pagination = (pageNumber)=>{
-  setCurrentPage(pageNumber)
-}
+    updateCurrentPokemon();
+  }, [currentPage, pokemons]);
 
-console.log("singlepokemon", singlePokemon)
+  useEffect(() => {
+    const fetchDataForPokemon = async (url, index) => {
+      const data = await fetchPokemonData(url);
+      setSinglePokemon((pokemonArray) => {
+        const newPokemonArray = [...pokemonArray];
+        newPokemonArray[index] = data;
+        return newPokemonArray;
+      });
+    };
+
+    currentPokemon.forEach((pokemon, index) => {
+      console.log("thats 1");
+      fetchDataForPokemon(pokemon.url, index);
+    });
+  }, [currentPokemon]);
+
+  const pagination = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const totalPokemonCount = pokemons.length;
+  const totalPageCount = totalPokemonCount / pokemonPerPage;
+
+  const generatePaginationNumbers = () => {
+    const paginationNumbers = [];
+    for (let i = 1; i <= totalPageCount; i++) {
+      paginationNumbers.push(
+        <div
+          key={i}
+          className={currentPage === i ? "active" : "nonActive"}
+          onClick={() => pagination(i)}
+        >
+          {i}
+        </div>
+      );
+    }
+    return paginationNumbers;
+  };
+
   return (
     <div className="App">
-     {currentPokemon.map((singlePokemon, index)=>(
-      <div key={singlePokemon.name}>
-        <h1>{singlePokemon.name}</h1>
-        <img alt={singlePokemon.name} src={singlePokemon.sprites.back_default}></img>
-        <div>Weight {singlePokemon.weight}</div>
-        <div>Height {singlePokemon.height}</div>
-      </div>
-     ))}
-      <div className='pagination'>
-        <button className='backPagination' onClick={()=>{
-           if(currentPage>1){
-            pagination(currentPage-1)
-           }
-        }
-         
-        }>&lt;&lt;&lt;</button>
-        <div className={currentPage===1? "active": "nonActive"}
-        onClick={()=>{pagination(1)}}>1</div>
-        <div className={currentPage===2? "active": "nonActive"}
-        onClick={()=>{pagination(2)}}>2</div>
-        <div className={currentPage===3? "active": "nonActive"}
-        onClick={()=>{pagination(3)}}>3</div>
-        <div className={currentPage===4? "active": "nonActive"}
-        onClick={()=>{pagination(4)}}>4</div>
-        <button className='forwardPagination' onClick={()=>{
-           if(currentPage<4){
-            pagination(currentPage+1)
-           }
-        }
-         
-        }>&gt;&gt;&gt;</button>
+      {singlePokemon.map((singlePokemon, index) => (
+        <div key={singlePokemon.name}>
+          <h1>{singlePokemon.name}</h1>
+          <img
+            alt={singlePokemon.name}
+            src={singlePokemon.sprites.back_default}
+          ></img>
+          <div>Weight {singlePokemon.weight}</div>
+          <div>Height {singlePokemon.height}</div>
+        </div>
+      ))}
+      <div className="pagination">
+        <button
+          className="backPagination"
+          onClick={() => {
+            if (currentPage > 1) {
+              pagination(currentPage - 1);
+            }
+          }}
+        >
+          &lt;&lt;&lt;
+        </button>
+        {generatePaginationNumbers()}
+        <button
+          className="forwardPagination"
+          onClick={() => {
+            if (currentPage < 4) {
+              pagination(currentPage + 1);
+            }
+          }}
+        >
+          &gt;&gt;&gt;
+        </button>
       </div>
     </div>
   );
-
-
 }
 
 export default App;
